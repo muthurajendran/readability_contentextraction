25d2e7030c2030f9339ad3cab169bb92eba69f51
Muthu <raju.spm@gmail.com>
2016-01-29 13:13:08 -0800

    adding calculating statistics file

--- scripts/get_stats.py
+++ scripts/get_stats.py
@@ -4,8 +4,32 @@
 ###############################
 
 from jaccard_similarity import jSimilarity
-import sys
+import sys,json
 
-tst = jSimilarity("hey dude","dude")
+if len(sys.argv) < 3:
+	print "Expecting 2 arguments"
+	print "Usage : <script.py> <gold_data_jsonfile> <processed_data_jsonfile>"
+	sys.exit()
 
-print tst
+gold_file = sys.argv[1] 
+processed_file = sys.argv[2]
+
+with open(gold_file) as data_file:    
+    gold_data = json.load(data_file)
+
+with open(processed_file) as data_file:    
+    processed_data = json.load(data_file)
+
+for gold in gold_data:
+	try:
+		processed_data_item = processed_data[str(gold['uid'])]
+		jacc_sim = jSimilarity(gold['required'] + gold['optional'],processed_data_item['content'])
+		print gold['uid'], jacc_sim, gold['id']
+		# print gold['required'] + gold['optional']
+		# print "*"*50
+		# print processed_data_item['content']
+		# print "*"*50
+	except Exception, e:
+		print "No id found with num "+ str(gold['uid']) +"! Skipping"
+
+
